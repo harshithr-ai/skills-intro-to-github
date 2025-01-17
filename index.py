32a0356e3940192969510025f7132e6c713e6a9b
harshithr-ai <harshith@v-align.in>
2025-01-17 16:50:34 +0530

    Added credit and pan card details

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ for num in list:
 
 print ("hello world")
 
-
 print("First EC2 conection from git to aws.")
 
 
@@ -16,3 +15,7 @@ print ("friday")
 
 print ("Cybersecyrity tests")
 
+print("Test codes")
+# Pan Card Number GAZPM3729R
+# Credit Card Number 4929-3813-3266-4295
+# Credit card number 5370-4638-8881-3020
