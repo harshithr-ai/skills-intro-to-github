8d2d51f273e2d949b980d2c5a52f79af218ab463
harshithr-ai <harshith@v-align.in>
2025-04-24 12:53:45 +0530

    This is my commit test and explanation.

--- index.py
+++ index.py
@@ -15,14 +15,9 @@ print ("friday")
 
 print ("Cybersecyrity tests")
 
-print("Test codes")
-# Pan Card Number GAZPM3729R4
-# Credit Card Number 4929-3813-3266-4295
-# Credit card number 5370-4638-8881-3020
 
 print ("python is a programming language")
-# Credit card number 5370-4638-8881-3021# new changes
 
-# Credit Card Number 4929-3813-3266-4297
+print ("New laptop 29/1/2025")
 
 print ("New laptop 29/1/2025")
